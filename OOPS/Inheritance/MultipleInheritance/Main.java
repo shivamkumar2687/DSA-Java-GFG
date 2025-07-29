67a43c3bc42cc37dda7846be101ce440f9094811
Shivam Kumar <shivamkumar2687@gmail.com>
2025-07-29 10:19:56 +0530

    Interface- MultipleInheritance

--- OOPS/Inheritance/MultipleInheritance/Main.java
+++ OOPS/Inheritance/MultipleInheritance/Main.java
@@ -1,3 +1,40 @@
+// The main reason Java doesn’t support multiple inheritance (with classes) 
+// is to avoid confusion or conflict in cases like:
+
+// The Diamond Problem
+
+// class A {
+//     void display() {
+//         System.out.println("A's display");
+//     }
+// }
+
+// class B extends A {
+//     void display() {
+//         System.out.println("B's display");
+//     }
+// }
+
+// class C extends A {
+//     void display() {
+//         System.out.println("C's display");
+//     }
+// }
+
+// // Now imagine: 
+// // class D extends B, C { }  // Not allowed in Java
+
+// ------
+// If both B and C override display(), and class D inherits from both, then:
+
+// D d = new D();
+// d.display(); // ❓ Which display() should it call? B's or C's?
+
+
+
+
+
+
 package OOPS.Inheritance.MultipleInheritance;
 
 interface  A{
@@ -14,12 +51,13 @@ interface B{
 class C implements A,B{
     @Override
     public  void greet(){
-        A.super.greet();
+        A.super.greet(); //Resolves the conflict
+        
     }
 }
 class Main{
     public static void main(String[] args) {
         C obj = new C();
-        obj.greet();
+        obj.greet(); // Output: Hello from A
     }
 }
